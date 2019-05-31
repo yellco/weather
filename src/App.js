a853a349dba1ca058d71f5407494fbcde69fe2c6
Dmitriy Zheltyy <dazheltyy@gmail.com>
2019-05-31 22:39:36 +0300

    Add some checks

--- src/App.js
+++ src/App.js
@@ -25,19 +25,30 @@ class App extends React.Component {
       const url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
       const data = await url.json();
 
-      let sunset = data.sys.sunset;
-      let date = new Date();
-      date.setTime(sunset);
-      let sunset_date = date.getHours() +  ":" + date.getMinutes() + ":" + date.getSeconds();
-
-      this.setState({
-        temp: data.main.temp,
-        city: data.name,
-        country: data.sys.country,
-        pressure: data.main.pressure,
-        sunset: sunset_date,
-        error: undefined
-      });
+      if (data.hasOwnProperty("message")) {
+        this.setState({
+          temp: undefined,
+          city: undefined, 
+          country: undefined,
+          pressure: undefined,
+          sunset: undefined,
+          error: "Неверно указан город!"
+        });
+      } else {
+        let sunset = data.sys.sunset;
+        let date = new Date();
+        date.setTime(sunset);
+        let sunset_date = date.getHours() +  ":" + date.getMinutes() + ":" + date.getSeconds();
+  
+        this.setState({
+          temp: data.main.temp,
+          city: data.name,
+          country: data.sys.country,
+          pressure: data.main.pressure,
+          sunset: sunset_date,
+          error: undefined
+        });
+      }
     } else {
       this.setState({
         temp: undefined,

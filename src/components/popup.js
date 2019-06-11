6f3f08cc3bf8ddf0afe1d212575ee8d61884b075
Zheltiy DA <zda@mogt.ru>
2019-06-11 16:30:05 +0300

    Add close svg for popup

--- src/components/popup.js
+++ src/components/popup.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import close from '../assets/icons/close.svg';
 
 export default class Popup extends Component {
     render() {
             return (
                 <div style={wrapper} onClick={this.props.closePopup}>
-                   <div className="Popup" style={timeClass} onClick={this.props.closePopup}>
-                        Выберите фон
+                   <div className="Popup" style={timeClass}>
+                        <div style={header}>
+                            Выберите фон
+                            <img src={close} alt='' style={img} onClick={this.props.closePopup} />
+                        </div>
                         <div style={items}>
                             <div style={item}>1</div>
                             <div style={item}>2</div>
@@ -19,6 +23,20 @@ export default class Popup extends Component {
             );
     }
 }
+const header = {
+    position: "relative",
+}
+
+const img = {
+    width: "20px",
+    height: "20px",
+    zIndex: "99999",
+    position: "absolute",
+    right: "5px",
+    top: "5px",
+    cursor: "pointer"
+}
+
 const items = {
     display: "flex",
     flexWrap: "wrap",
